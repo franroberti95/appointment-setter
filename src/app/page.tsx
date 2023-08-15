2f21d57fec28d4df1632b375cc6c6d6979fe1b57
franco <franco.roberti@truenorth.co>
2023-08-14 21:14:53 -0300

    Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,65 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={styles.top}>
+        <Image src={'/imgs/logotipo.png'} alt={'logo'} width={300} height={55.6}/>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <h2 >
+          De $200 a $2000usd en un mes con solo un APPOINMENT SETTER.
+        </h2>
+
+          <div className={styles.videoContainer}>
+
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/Jojgmcx5Q_c" title="YouTube video player"
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      allowFullScreen></iframe>
+          </div>
+
+        <p>
+          Escalá tu negocio un un X3 en solo tres meses con UN SOLO APPOINTMENT SETTER. Acá te contamos cómo escalamos un X10 en nuestro negocio sin invertir un solo centavo y sin pagar un solo sueldo, solamente con un Appointment Setter en nuestra cuenta.
+        </p>
+
+          <div className={styles.testimonials}>
+
+              {
+                  [
+                      '/testimonios/1.PNG',
+                      '/testimonios/2.PNG',
+                      '/testimonios/3.PNG',
+                      '/testimonios/4.PNG',
+                      '/testimonios/5.PNG',
+                      '/testimonios/6.PNG',
+                      '/testimonios/7.PNG',
+                      '/testimonios/8.PNG',
+                      '/testimonios/9.PNG',
+                      '/testimonios/10.jpeg',
+                  ]
+                      .map( i =>
+                          <Image
+                              key={i}
+                              src={i}
+                              alt={i}
+                              width={300}
+                              height={600}
+                          />
+                      )
+              }
+          </div>
+
+
+          <div className="calendly-inline-widget" data-url="https://calendly.com/starmediaworkteam/30min" style={{minWidth:320, height:700}}/>
+          <Script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></Script>
+
+
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
